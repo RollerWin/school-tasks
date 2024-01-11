777d0ab423263e6bdefb62b220b4b98be8af30d2
RollerWin <rinatyych@mail.ru>
2024-01-11 13:53:06 +0300

    fixed solution

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿const int PICTURES_IN_ROW = 3;
-
+﻿int picturesInRow = 3;
 int numberOfPictures = 52;
 
-Console.WriteLine($"Из {numberOfPictures} картин получилось {numberOfPictures / PICTURES_IN_ROW} рядов.\nСверх меры оказалось {numberOfPictures % PICTURES_IN_ROW} картин");
+int numberOfRows = numberOfPictures / picturesInRow;
+int picturesOverflow = numberOfPictures % picturesInRow;
+
+Console.WriteLine($"Из {numberOfPictures} картин получилось {numberOfRows} рядов.\nСверх меры оказалось {picturesOverflow} картин");
