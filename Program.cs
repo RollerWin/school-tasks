d25b295464c6b95ebdb320bb51da4529062a4c9e
RollerWin <rinatyych@mail.ru>
2024-01-11 14:24:50 +0300

    added solution

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-﻿string name = "Petrov";
-string surname = "Vasiliy";
+﻿int playerCoins;
+int playerBag = 0;
+int numberOfDiamonds = 20;
+int diamondPrice = 5;
 
-Console.WriteLine($"Ваше имя: {name}\nВаша фамилия: {surname}");
-Console.WriteLine("Хм... Что-то не так...");
+Console.Write("Укажите ваше количество монет: ");
+playerCoins = Convert.ToInt32(Console.ReadLine());
 
-string wordBuffer = name;
-name = surname;
-surname = wordBuffer;
+Console.Write($"У вас {playerCoins} монет!\nНа рынке {numberOfDiamonds} алмазов\nСколько алмазов желаете приобрести?\t");
+int diamondsToBuy = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine($"Теперь должно быть всё верно..\nВаше имя: {name}\nВаша фамилия: {surname}");
+bool canBuyDiamonds = numberOfDiamonds >= diamondsToBuy;
+
+int numberOfPurchasedDiamonds = diamondsToBuy * Convert.ToInt32(canBuyDiamonds);
+
+int totalCost = diamondPrice * numberOfPurchasedDiamonds;
+playerCoins -= totalCost;
+numberOfDiamonds -= numberOfPurchasedDiamonds;
+playerBag += numberOfPurchasedDiamonds;
+
+Console.WriteLine($"Сделка состоялась!\nТеперь у Вас {playerCoins} монет\nНа рынке {numberOfDiamonds} алмазов\nА у вас теперь {playerBag} алмазов!");
