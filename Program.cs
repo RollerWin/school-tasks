8fe807ab2b6a228a745824a34f09569b5ba00751
RollerWin <rinatyych@mail.ru>
2024-01-11 13:28:49 +0300

    added new solve

--- Program.cs
+++ Program.cs
@@ -1,12 +1,5 @@
-﻿string userName;
-int userAge;
-string userZodiacSign;
+﻿const int PICTURES_IN_ROW = 3;
 
-Console.WriteLine("Как вас зовут?");
-userName = Console.ReadLine();
-Console.WriteLine("Сколько Вам лет?");
-userAge = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Какой у Вас знак зодиака?");
-userZodiacSign = Console.ReadLine();
+int numberOfPictures = 52;
 
-Console.WriteLine($"Всё ясно!\nВас зовут {userName}, Вам {userAge} лет и вы по гороскопу {userZodiacSign}");
+Console.WriteLine($"Из {numberOfPictures} картин получилось {numberOfPictures / PICTURES_IN_ROW} рядов.\nСверх меры оказалось {numberOfPictures % PICTURES_IN_ROW} картин");
