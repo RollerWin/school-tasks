54553fcff4e5b261d5e85a18c9248833c8e812e8
RollerWin <rinatyych@mail.ru>
2024-01-13 22:45:38 +0300

    fixed solution

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
 ﻿string systemPassword = "qwerty";
-bool isAuthorized = false;
 int numberOfAttempts = 3;
 string secretMessage = "aboba";
 string userInput;
 
-while(!isAuthorized && numberOfAttempts > 0)
+for(int i = numberOfAttempts - 1; i >= 0; i--)
 {
     Console.Write("Введите пароль: ");
     userInput = Console.ReadLine();
 
     if(userInput == systemPassword)
     {
-        isAuthorized = true;
         Console.WriteLine($"Пароль верный!\nСекретное сообщение: {secretMessage}");
     }
     else
     {
-        numberOfAttempts--;
-        Console.WriteLine($"Неверный пароль!\nУ вас осталось {numberOfAttempts} попыток");
+        Console.WriteLine($"Неверный пароль!\nУ вас осталось {i} попыток");
     }
-}
-
-if(numberOfAttempts == 0)
-{
-    Console.WriteLine("Увы, количество попыток закончилось! Система выключается...");
 }
