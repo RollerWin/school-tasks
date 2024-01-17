7578a5e3b3588ac3652c360b4c6734351cffafe2
RollerWin <rinatyych@mail.ru>
2024-01-17 05:32:03 +0300

    added solution

--- Program.cs
+++ Program.cs
@@ -1,37 +1,81 @@
-﻿char openBracket = '(';
-char closeBracket = ')';
-int bracketCounter = 0;
+﻿using System.Runtime.Serialization;
 
-string correctResultMessage = "Выражение верно!";
-string incorrectResultMessage = "Неверное выражение!";
+const string useRashamonSpellCommand = "1";
+const string useHuganzakuraSpellCommand = "2";
+const string useHealingSpellCommand = "3";
+const string useDamageSpellCommand = "4";
 
-bool isCorrect = true;
+double userHealth = 100.0;
 
-Console.Write("Введите скобочное выражение: ");
-string userInput = Console.ReadLine();
+double rashamonSpellCoefficientDamage = 0.9;
 
-foreach(var symbol in userInput)
+double huganzakuraSpellDamage = 35.0;
+
+double healingSpellCoefficient = 1.25;
+
+double damageSpell = 15.0;
+
+double enemyHealth = 200.0;
+double enemyDamage = 20.0;
+
+bool isPhantomSummoned = false;
+bool canUserBeAttacked = true;
+
+while(userHealth > 0.0 && enemyHealth > 0.0)
 {
-    if(symbol == openBracket)
+    canUserBeAttacked = true;
+
+    Console.Clear();
+    Console.WriteLine($"\tАрена!\nЗдоровье игрока: {}\nЗдоровье врага: {enemyHealth}");
+    Console.WriteLine($"Выберите действие:\n{useRashamonSpellCommand} - Призвать духа-помощника\n{useHuganzakuraSpellCommand} - Нанести урон духом\n{useHealingSpellCommand} - Восстановить здоровье\n{useDamageSpellCommand} - Нанести урон самому");
+
+    switch(Console.ReadLine())
     {
-        bracketCounter++;
-    }
-    else if(symbol == closeBracket)
-    {
-        bracketCounter--;
-    }
-    else
-    {
-        isCorrect = false;
+        case useRashamonSpellCommand:
+            isPhantomSummoned = true;
+            userHealth = Math.Round(userHealth * rashamonSpellCoefficientDamage, 2);
         break;
+
+        case useHuganzakuraSpellCommand:
+            if(isPhantomSummoned == true)
+            {
+                enemyHealth -= huganzakuraSpellDamage;
+            }
+            else
+            {
+                Console.WriteLine("Вы не призвали духа!");
+            }
+        break;
+
+        case useHealingSpellCommand:
+            canUserBeAttacked = false;
+            userHealth = Math.Round(userHealth * healingSpellCoefficient, 2);
+        break;
+
+        case useDamageSpellCommand:
+            enemyHealth -= damageSpell;
+        break;
+
+        default:
+            Console.WriteLine("Неверная команда!");
+        break;
+    }
+
+    if(canUserBeAttacked == true)
+    {
+        userHealth = Math.Round(userHealth - enemyDamage, 2);
     }
 }
 
-if(bracketCounter == 0 && isCorrect == true)
+if(userHealth <= 0)
 {
-    Console.WriteLine(correctResultMessage);
+    Console.WriteLine("Враг победил!");
+}
+else if(enemyHealth <= 0)
+{
+    Console.WriteLine("Игрок победил!");
 }
 else
 {
-    Console.WriteLine(incorrectResultMessage);
+    Console.WriteLine("Ничья!");
 }
