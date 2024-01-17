9330443e1c1bfde122f067f8ae460694371ca821
RollerWin <rinatyych@mail.ru>
2024-01-17 16:25:30 +0300

    fixed solution

--- Program.cs
+++ Program.cs
@@ -1,16 +1,12 @@
-﻿const string useRashamonSpellCommand = "1";
-const string useHuganzakuraSpellCommand = "2";
-const string useHealingSpellCommand = "3";
-const string useDamageSpellCommand = "4";
+﻿const string CommandUseRashamonSpell = "1";
+const string CommandUseHuganzakuraSpell = "2";
+const string CommandUseHealingSpell = "3";
+const string CommandUseDamageSpell = "4";
 
 double userHealth = 100.0;
-
 double rashamonSpellCoefficientDamage = 0.9;
-
 double huganzakuraSpellDamage = 35.0;
-
 double healingSpellCoefficient = 1.25;
-
 double damageSpell = 15.0;
 
 double enemyHealth = 200.0;
@@ -19,22 +15,24 @@ double enemyDamage = 20.0;
 bool isPhantomSummoned = false;
 bool canUserBeAttacked = true;
 
+int numberOfDecimalPlaces = 2;
+
 while(userHealth > 0.0 && enemyHealth > 0.0)
 {
     canUserBeAttacked = true;
 
     Console.Clear();
-    Console.WriteLine($"\tАрена!\nЗдоровье игрока: {}\nЗдоровье врага: {enemyHealth}");
-    Console.WriteLine($"Выберите действие:\n{useRashamonSpellCommand} - Призвать духа-помощника\n{useHuganzakuraSpellCommand} - Нанести урон духом\n{useHealingSpellCommand} - Восстановить здоровье\n{useDamageSpellCommand} - Нанести урон самому");
+    Console.WriteLine($"\tАрена!\nЗдоровье игрока: {userHealth}\nЗдоровье врага: {enemyHealth}");
+    Console.WriteLine($"Выберите действие:\n{CommandUseRashamonSpell} - Призвать духа-помощника\n{CommandUseHuganzakuraSpell} - Нанести урон духом\n{CommandUseHealingSpell} - Восстановить здоровье\n{CommandUseDamageSpell} - Нанести урон самому");
 
     switch(Console.ReadLine())
     {
-        case useRashamonSpellCommand:
+        case CommandUseRashamonSpell:
             isPhantomSummoned = true;
-            userHealth = Math.Round(userHealth * rashamonSpellCoefficientDamage, 2);
+            userHealth = Math.Round(userHealth * rashamonSpellCoefficientDamage, numberOfDecimalPlaces);
         break;
 
-        case useHuganzakuraSpellCommand:
+        case CommandUseHuganzakuraSpell:
             if(isPhantomSummoned == true)
             {
                 enemyHealth -= huganzakuraSpellDamage;
@@ -45,12 +43,12 @@ while(userHealth > 0.0 && enemyHealth > 0.0)
             }
         break;
 
-        case useHealingSpellCommand:
+        case CommandUseHealingSpell:
             canUserBeAttacked = false;
-            userHealth = Math.Round(userHealth * healingSpellCoefficient, 2);
+            userHealth = Math.Round(userHealth * healingSpellCoefficient, numberOfDecimalPlaces);
         break;
 
-        case useDamageSpellCommand:
+        case CommandUseDamageSpell:
             enemyHealth -= damageSpell;
         break;
 
@@ -61,19 +59,19 @@ while(userHealth > 0.0 && enemyHealth > 0.0)
 
     if(canUserBeAttacked == true)
     {
-        userHealth = Math.Round(userHealth - enemyDamage, 2);
+        userHealth = Math.Round(userHealth - enemyDamage, numberOfDecimalPlaces);
     }
 }
 
-if(userHealth <= 0)
+if(userHealth <= 0 && enemyHealth <= 0)
 {
-    Console.WriteLine("Враг победил!");
+    Console.WriteLine("Ничья!");
 }
-else if(enemyHealth <= 0)
+else if(userHealth > 0)
 {
     Console.WriteLine("Игрок победил!");
 }
 else
 {
-    Console.WriteLine("Ничья!");
+    Console.WriteLine("Враг победил!");
 }
